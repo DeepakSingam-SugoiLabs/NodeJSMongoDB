81f889bd1878f0a87852bffb453014407f279604
unknown <deepak@sugoilabs.com>
2021-02-26 08:39:51 +0530

    add picture

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,7 @@
 const Post = require('../models/post')
 const formiable = require('formidable')
 const fs = require('fs')
+const path = require('path') 
 exports.getPosts = (req,res) => {
     // const posts = Post.find().select("_id email password")
     const posts = Post.find().populate("postedBy","_id ")
@@ -28,9 +29,24 @@ form.parse(req,(err,fields,files)=>{
         
         let post = new Post(fields)
         post.postedBy = req.profile
-        if(files.photo){
+        console.log("fields are",fields)
+        if(files.photo && fields.title && fields.body){
             post.photo.data = fs.readFileSync(files.photo.path)
             post.photo.contentType = files.photo.type
+            let oldPath = files.photo.path;
+            var newPath = path.join(__dirname, '../uploads') 
+            + '/'+files.photo.name 
+            var rawData = fs.readFileSync(oldPath) 
+            fs.writeFile(newPath, rawData, function(err){ 
+                if(err) console.log(err) 
+                return ""
+            }) 
+            console.log("picture is",post.files)
+        }
+        else{
+            return res.status(400).json({
+                error:"Image could not be uploaded"
+            })
         }
         post.save((err,result)=>{
             if(err){
